180758052e68cb75505c3ba093427b8e79c988f1
diswho <discover.who@gmail.com>
2024-02-27 14:39:10 +0700

    re-order

--- test.py
+++ test.py
@@ -20,24 +20,31 @@ Base.metadata.create_all(bind=engLOC)
 
 try:
     sesLOC = SesLOC()
-    descendant_id=[]
-    child_dept=[]
+    descendant_id = []
+    child_dept = []
+
     def descendant_list(dept_code, indent=0):
-        list = sesLOC.query(HRDepartment).filter_by(dept_parentcode=dept_code).all()
+        list = sesLOC.query(HRDepartment).filter_by(
+            dept_parentcode=dept_code).all()
         # result.append(dept_code)
         for child_depart in list:
             descendant_id.append(child_depart.id)
             # print(result)
             # print('\t' * indent + f'i:{child_depart.id} \tc:{child_depart.dept_code} \tn:{child_depart.dept_name}')
             descendant_list(child_depart.dept_code, indent+1)
-    descendant_list(0)
-    print(descendant_id)
-    def child_dept_code(dept_code):        
-        list = sesLOC.query(HRDepartment).filter_by(dept_parentcode=dept_code).all()
+
+    def child_dept_code(dept_code):
+        list = sesLOC.query(HRDepartment).filter_by(
+            dept_parentcode=dept_code).all()
         for child_list in list:
             child_dept.append(child_list.dept_code)
-    child_dept_code(0)
-    print(child_dept)
+
+    depart_code=33
+    # 1, 17, 19, 20, 21, 27, 33, 90, 124, 155, 172, 181
+    descendant_list(depart_code)
+    child_dept_code(depart_code)
+    print(f'descendant id: {descendant_id}')
+    print(f'child depart code: {child_dept}')
 
     # def print_department_tree(department, indent=0):
     #     print('-' * indent + str(department.id) + '=' +
