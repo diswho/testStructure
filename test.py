a1337fb749690ccd57575b28e1f50ca4e996fb26
diswho <discover.who@gmail.com>
2024-02-28 11:29:53 +0700

    28

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ from sqlalchemy.sql import case
 from app.model import HRDepartment
 from app.db.session import Base
 
+import json
+
 DB_LOCAL = "sqlite:///./local.db"
 # DB_EXTRA = r"sqlite:///C:\\Users\\phuong\\OneDrive\\Private\\Xokthavi\\HR\\ZKTimeNet.db"
 # Create an engine
@@ -23,28 +25,39 @@ try:
     descendant_id = []
     child_dept = []
 
+    def get_tree(dept_code=0, indent=0):
+        list = sesLOC.query(HRDepartment).filter_by(
+            dept_parentcode=dept_code).all()
+        for child_depart in list:
+            print('  ---  ' * indent + f'{child_depart.dept_name}')
+            get_tree(child_depart.dept_code, indent+1)
+
     def descendant_list(dept_code, indent=0):
         list = sesLOC.query(HRDepartment).filter_by(
             dept_parentcode=dept_code).all()
-        # result.append(dept_code)
         for child_depart in list:
             descendant_id.append(child_depart.id)
-            # print(result)
-            # print('\t' * indent + f'i:{child_depart.id} \tc:{child_depart.dept_code} \tn:{child_depart.dept_name}')
             descendant_list(child_depart.dept_code, indent+1)
 
     def child_dept_code(dept_code):
         list = sesLOC.query(HRDepartment).filter_by(
             dept_parentcode=dept_code).all()
         for child_list in list:
-            child_dept.append(child_list.dept_code)
+            child_dept.append(child_list)
+            # child_dept.append(child_list.dept_code)
 
-    depart_code=33
+    depart_code = 172
     # 1, 17, 19, 20, 21, 27, 33, 90, 124, 155, 172, 181
+    prt = sesLOC.query(HRDepartment).filter_by(dept_code=depart_code).first()
+    if prt:
+        descendant_id.append(prt.id)
+    get_tree()
     descendant_list(depart_code)
     child_dept_code(depart_code)
     print(f'descendant id: {descendant_id}')
-    print(f'child depart code: {child_dept}')
+    for chd in child_dept:
+        print(
+            f'child depart id: {chd.id}, \tcode: {chd.dept_code},\t name: {chd.dept_name}')
 
     # def print_department_tree(department, indent=0):
     #     print('-' * indent + str(department.id) + '=' +
